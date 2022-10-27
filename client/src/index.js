6771368c3878ae1427376210503ab493ba34529c
emuller1996 <stefano.muller@hotmail.com>
2022-10-26 22:13:57 -0500

    config para deply

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./store/index";
+import axios from "axios";
+
+
+axios.defaults.baseURL = process.env.REACT_APP_API;
 
 ReactDOM.render(
   <Provider store={store}>
